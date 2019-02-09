520b86c42dc87ff135f9a5fceb7df2d136b0c718
Michael <msiller455@gmail.com>
2019-02-08 17:02:12 -0800

    make vendor model

---
+++ models/vendors.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const vendorSchema = mongoose.Schema({
+    email: String,
+    password: String,
+    vendorName: String,
+    location: String,
+    image: String,
+    schedule: [String],
+    category: String,
+    phone: Number,
+    website: String
+})
+
+const Vendor = mongoose.model('Vendor', vendorSchema)
+
+module.exports = Vendor
