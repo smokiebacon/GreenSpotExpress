19324d66978f9fa896fcae42e2df1668591d8f85
Brent Ahrens <cidida@gmail.com>
2019-02-12 15:37:45 -0800

    brent: data-setup

--- routes/authController.js
+++ routes/authController.js
@@ -1,11 +1,7 @@
 exports.google = (req, res) => {
     const io = req.app.get('io')
     const user = {
-      name: req.user.displayName,
-      photo: req.user.photos[0].value.replace(/sz=50/gi, 'sz=250')
+      name: req.user.username
     }
-    console.log(user)
-    console.log(req.session)
-    console.log(io.in())
     io.in(req.session.socketId).emit('google', user)
    }
