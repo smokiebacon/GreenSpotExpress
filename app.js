56b3cf43f118e10b6c5117a58c0f4a4228fe00ea
Michael <msiller455@gmail.com>
2019-02-14 14:12:58 -0800

    getting heroku launched

--- app.js
+++ app.js
@@ -75,10 +75,12 @@ if(!isProduction) {
   app.use(errorHandler())
 }
 
-app.use('/', authRouter);
+
+app.use('/google', authRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
